837dcdbf68b4a52aeccad83558042a2578b8a863
likenhp <likenhp@gmail.com>
2020-07-31 16:46:28 -0700

    added routing

--- routing-practice/src/app/app-routing.module.ts
+++ routing-practice/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Route, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SettingsComponent } from './settings/settings.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Route[] = [
+  {path: '', redirectTo: '/home', pathMatch: 'full' }, // pathMatch can also be prefix
+  {path: 'home', component: HomeComponent },
+  {path: 'settings',  component: SettingsComponent },
+  {path: '**',  component: PageNotFoundComponent }, // ** are wildcards, means matching everything
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
