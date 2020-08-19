d8d63889b6b7ad0ab8a09c4ee650dd6710cdffc0
likenhp <likenhp@gmail.com>
2020-08-19 13:36:33 -0700

    testing observables

--- observables-practice/src/app/app.module.ts
+++ observables-practice/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StreamObjectComponent } from './stream-object/stream-object.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StreamObjectComponent
   ],
   imports: [
     BrowserModule,
---
+++ observables-practice/src/app/stream-object/stream-object.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stream-object',
+  templateUrl: './stream-object.component.html',
+  styleUrls: ['./stream-object.component.css']
+})
+export class StreamObjectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
